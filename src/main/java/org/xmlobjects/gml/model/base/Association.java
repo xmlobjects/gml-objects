f43016489c84021419bbac79e644808ec1ee24b2
Claus Nagel <cnagel@virtualcitysystems.de>
2019-07-11 20:32:32 +0200

    reworked associations

---
+++ src/main/java/org/xmlobjects/gml/model/base/Association.java
@@ -0,0 +1,17 @@
+package org.xmlobjects.gml.model.base;
+
+import org.xmlobjects.gml.model.GMLObject;
+import org.xmlobjects.gml.model.common.LocalProperties;
+
+public abstract class Association<T extends GMLObject> extends GMLObject {
+    private LocalProperties localProperties;
+
+    public abstract Class<T> getTargetType();
+
+    public LocalProperties getLocalProperties() {
+        if (localProperties == null)
+            localProperties = new LocalProperties();
+
+        return localProperties;
+    }
+}
