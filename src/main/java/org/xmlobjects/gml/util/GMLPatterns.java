4d1ce212b7d8f5de449cb043f0a364c0f2f141cd
Claus Nagel <cnagel@virtualcitysystems.de>
2019-07-31 23:07:58 +0200

    minor change

--- src/main/java/org/xmlobjects/gml/util/GMLPatterns.java
+++ src/main/java/org/xmlobjects/gml/util/GMLPatterns.java
@@ -5,6 +5,6 @@ import java.util.regex.Pattern;
 public class GMLPatterns {
     public static final Pattern UOM_SYMBOL = Pattern.compile("[^: \\n\\r\\t]+");
     public static final Pattern UOM_URI = Pattern.compile("([a-zA-Z][a-zA-Z0-9\\-+.]*:|\\.\\./|\\./|#).*");
-    public static final Pattern AXIS_ORDER = Pattern.compile("[+\\-][1-9][0-9]*");
+    public static final Pattern AXIS_DIRECTION = Pattern.compile("[+\\-][1-9][0-9]*");
     public static final Pattern OTHER_VALUE = Pattern.compile("other:\\w{2,}");
 }
