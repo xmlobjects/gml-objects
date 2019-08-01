bd89b5be55971e1bcf39aa69c3ee1c9aa0948b91
Claus Nagel <cnagel@virtualcitysystems.de>
2019-08-01 22:36:43 +0200

    minor change

--- src/main/java/org/xmlobjects/gml/adapter/basictypes/CodeOrNilReasonListAdapter.java
+++ src/main/java/org/xmlobjects/gml/adapter/basictypes/CodeOrNilReasonListAdapter.java
@@ -1,10 +1,13 @@
 package org.xmlobjects.gml.adapter.basictypes;
 
+import org.xmlobjects.builder.ObjectBuildException;
 import org.xmlobjects.builder.ObjectBuilder;
 import org.xmlobjects.gml.model.basictypes.CodeOrNilReasonList;
+import org.xmlobjects.serializer.ObjectSerializeException;
 import org.xmlobjects.serializer.ObjectSerializer;
 import org.xmlobjects.stream.XMLReadException;
 import org.xmlobjects.stream.XMLReader;
+import org.xmlobjects.stream.XMLWriteException;
 import org.xmlobjects.stream.XMLWriter;
 import org.xmlobjects.xml.Attributes;
 import org.xmlobjects.xml.Element;
@@ -13,7 +16,6 @@ import org.xmlobjects.xml.Namespaces;
 import javax.xml.namespace.QName;
 
 public class CodeOrNilReasonListAdapter implements ObjectBuilder<CodeOrNilReasonList>, ObjectSerializer<CodeOrNilReasonList> {
-    private final NameOrNilReasonListAdapter delegate = new NameOrNilReasonListAdapter();
 
     @Override
     public CodeOrNilReasonList createObject(QName name) {
@@ -21,14 +23,14 @@ public class CodeOrNilReasonListAdapter implements ObjectBuilder<CodeOrNilReason
     }
 
     @Override
-    public void initializeObject(CodeOrNilReasonList object, QName name, Attributes attributes, XMLReader reader) throws XMLReadException {
-        delegate.initializeObject(object, name, attributes, reader);
+    public void initializeObject(CodeOrNilReasonList object, QName name, Attributes attributes, XMLReader reader) throws ObjectBuildException, XMLReadException {
+        reader.getOrCreateBuilder(NameOrNilReasonListAdapter.class).initializeObject(object, name, attributes, reader);
         attributes.getValue("codeSpace").ifPresent(object::setCodeSpace);
     }
 
     @Override
-    public void initializeElement(Element element, CodeOrNilReasonList object, Namespaces namespaces, XMLWriter writer) {
-        delegate.initializeElement(element, object, namespaces, writer);
+    public void initializeElement(Element element, CodeOrNilReasonList object, Namespaces namespaces, XMLWriter writer) throws ObjectSerializeException, XMLWriteException {
+        writer.getOrCreateSerializer(NameOrNilReasonListAdapter.class).initializeElement(element, object, namespaces, writer);
         element.addAttribute("codeSpace", object.getCodeSpace());
     }
 }
--- src/main/java/org/xmlobjects/gml/adapter/basictypes/MeasureOrNilReasonListAdapter.java
+++ src/main/java/org/xmlobjects/gml/adapter/basictypes/MeasureOrNilReasonListAdapter.java
@@ -1,10 +1,13 @@
 package org.xmlobjects.gml.adapter.basictypes;
 
+import org.xmlobjects.builder.ObjectBuildException;
 import org.xmlobjects.builder.ObjectBuilder;
 import org.xmlobjects.gml.model.basictypes.MeasureOrNilReasonList;
+import org.xmlobjects.serializer.ObjectSerializeException;
 import org.xmlobjects.serializer.ObjectSerializer;
 import org.xmlobjects.stream.XMLReadException;
 import org.xmlobjects.stream.XMLReader;
+import org.xmlobjects.stream.XMLWriteException;
 import org.xmlobjects.stream.XMLWriter;
 import org.xmlobjects.xml.Attributes;
 import org.xmlobjects.xml.Element;
@@ -13,7 +16,6 @@ import org.xmlobjects.xml.Namespaces;
 import javax.xml.namespace.QName;
 
 public class MeasureOrNilReasonListAdapter implements ObjectBuilder<MeasureOrNilReasonList>, ObjectSerializer<MeasureOrNilReasonList> {
-    private final DoubleOrNilReasonListAdapter delegate = new DoubleOrNilReasonListAdapter();
 
     @Override
     public MeasureOrNilReasonList createObject(QName name) {
@@ -21,14 +23,14 @@ public class MeasureOrNilReasonListAdapter implements ObjectBuilder<MeasureOrNil
     }
 
     @Override
-    public void initializeObject(MeasureOrNilReasonList object, QName name, Attributes attributes, XMLReader reader) throws XMLReadException {
-        delegate.initializeObject(object, name, attributes, reader);
+    public void initializeObject(MeasureOrNilReasonList object, QName name, Attributes attributes, XMLReader reader) throws ObjectBuildException, XMLReadException {
+        reader.getOrCreateBuilder(DoubleOrNilReasonListAdapter.class).initializeObject(object, name, attributes, reader);
         attributes.getValue("uom").ifPresent(object::setUom);
     }
 
     @Override
-    public void initializeElement(Element element, MeasureOrNilReasonList object, Namespaces namespaces, XMLWriter writer) {
-        delegate.initializeElement(element, object, namespaces, writer);
+    public void initializeElement(Element element, MeasureOrNilReasonList object, Namespaces namespaces, XMLWriter writer) throws ObjectSerializeException, XMLWriteException {
+        writer.getOrCreateSerializer(DoubleOrNilReasonListAdapter.class).initializeElement(element, object, namespaces, writer);
         element.addAttribute("uom", object.getUom());
     }
 }
