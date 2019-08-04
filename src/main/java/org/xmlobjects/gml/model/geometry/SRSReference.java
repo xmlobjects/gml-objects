802002fc8e5cd4ed29608cfdaeae36b3766ac8c6
Claus Nagel <cnagel@virtualcitysystems.de>
2019-08-04 17:36:46 +0200

    minor change

--- src/main/java/org/xmlobjects/gml/model/geometry/SRSReference.java
+++ src/main/java/org/xmlobjects/gml/model/geometry/SRSReference.java
@@ -1,6 +1,7 @@
 package org.xmlobjects.gml.model.geometry;
 
 import org.xmlobjects.gml.model.GMLObject;
+import org.xmlobjects.gml.model.common.Child;
 import org.xmlobjects.gml.model.feature.AbstractFeature;
 
 public interface SRSReference extends SRSInformation {
@@ -10,19 +11,20 @@ public interface SRSReference extends SRSInformation {
     void setSrsDimension(Integer srsDimension);
 
     default SRSReference getInheritedSRSReference() {
-        if (getSrsName() == null && this instanceof GMLObject) {
-            GMLObject parent = (GMLObject) this;
-
-            while ((parent = parent.getParent()) != null) {
-                if (parent instanceof AbstractGeometry)
-                    return ((AbstractGeometry) parent).getInheritedSRSReference();
-                else if (parent instanceof AbstractFeature) {
-                    AbstractFeature feature = (AbstractFeature) parent;
-                    if (feature.getBoundedBy() != null
-                            && feature.getBoundedBy().getEnvelope() != null
-                            && feature.getBoundedBy().getEnvelope().getSrsName() != null)
-                        return feature.getBoundedBy().getEnvelope();
-                }
+        if (getSrsName() == null && this instanceof Child) {
+            GMLObject parent = ((Child) this).getParent();
+            if (parent != null) {
+                do {
+                    if (parent instanceof AbstractGeometry)
+                        return ((AbstractGeometry) parent).getInheritedSRSReference();
+                    else if (parent instanceof AbstractFeature) {
+                        AbstractFeature feature = (AbstractFeature) parent;
+                        if (feature.getBoundedBy() != null
+                                && feature.getBoundedBy().getEnvelope() != null
+                                && feature.getBoundedBy().getEnvelope().getSrsName() != null)
+                            return feature.getBoundedBy().getEnvelope();
+                    }
+                } while ((parent = parent.getParent()) != null);
             }
         }
 
