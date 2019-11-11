58502ccc09ccdb019baf60e4cc141fe985936963
Claus Nagel <cnagel@virtualcitysystems.de>
2019-11-11 18:15:27 +0100

    minor change

--- src/main/java/org/xmlobjects/gml/adapter/temporal/TimePositionAdapter.java
+++ src/main/java/org/xmlobjects/gml/adapter/temporal/TimePositionAdapter.java
@@ -71,7 +71,10 @@ public class TimePositionAdapter implements ObjectBuilder<TimePosition>, ObjectS
         }
 
         element.addAttribute("calendarEraName", object.getCalendarEraName());
-        element.addAttribute("frame", object.getFrame());
+
+        if (object.isSetFrame())
+            element.addAttribute("frame", object.getFrame());
+
         if (object.getIndeterminatePosition() != null)
             element.addAttribute("indeterminatePosition", object.getIndeterminatePosition().toValue());
     }
