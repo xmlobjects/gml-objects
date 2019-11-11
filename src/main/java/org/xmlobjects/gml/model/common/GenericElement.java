7d438064f480aa995ddc1150ade8383c8616a233
Claus Nagel <cnagel@virtualcitysystems.de>
2019-11-11 18:06:46 +0100

    minor change

--- src/main/java/org/xmlobjects/gml/model/common/GenericElement.java
+++ src/main/java/org/xmlobjects/gml/model/common/GenericElement.java
@@ -2,11 +2,16 @@ package org.xmlobjects.gml.model.common;
 
 import org.w3c.dom.Element;
 import org.xmlobjects.gml.model.GMLObject;
+import org.xmlobjects.util.copy.CopyBuilder;
+import org.xmlobjects.util.copy.Copyable;
 
 import java.util.Objects;
 
 public class GenericElement extends GMLObject {
     private Element content;
+
+    private GenericElement() {
+    }
 
     private GenericElement(Element content) {
         this.content = Objects.requireNonNull(content, "Content must not be null.");
@@ -26,5 +31,10 @@ public class GenericElement extends GMLObject {
 
     public String getNamespaceURI() {
         return content.getNamespaceURI();
+    }
+
+    @Override
+    public Copyable deepCopy(CopyBuilder builder) {
+        return super.deepCopy(builder.withClone(content, () -> content.cloneNode(true)));
     }
 }
