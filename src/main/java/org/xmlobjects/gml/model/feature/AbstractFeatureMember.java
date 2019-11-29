14da0e80ee446749886ddc326338ed4205660d76
Claus Nagel <cnagel@virtualcitysystems.de>
2019-11-29 18:03:49 +0100

    minor change

--- src/main/java/org/xmlobjects/gml/model/feature/AbstractFeatureMember.java
+++ src/main/java/org/xmlobjects/gml/model/feature/AbstractFeatureMember.java
@@ -1,13 +1,45 @@
 package org.xmlobjects.gml.model.feature;
 
 import org.xmlobjects.gml.model.base.AbstractInlineProperty;
+import org.xmlobjects.gml.model.common.GenericElement;
 
 public abstract class AbstractFeatureMember<T extends AbstractFeature> extends AbstractInlineProperty<T> {
+    private GenericElement genericElement;
 
     public AbstractFeatureMember() {
     }
 
     public AbstractFeatureMember(T object) {
         super(object);
+    }
+
+    public AbstractFeatureMember(GenericElement element) {
+        setGenericElement(element);
+    }
+
+    @Override
+    public T getObject() {
+        return super.getObject();
+    }
+
+    public boolean isSetObject() {
+        return getObject() != null;
+    }
+
+    @Override
+    public void setObject(T object) {
+        super.setObject(object);
+    }
+
+    public GenericElement getGenericElement() {
+        return genericElement;
+    }
+
+    public boolean isSetGenericElement() {
+        return genericElement != null;
+    }
+
+    public void setGenericElement(GenericElement genericElement) {
+        this.genericElement = asChild(genericElement);
     }
 }
