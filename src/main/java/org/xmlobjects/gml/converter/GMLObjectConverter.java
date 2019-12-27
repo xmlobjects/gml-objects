9730de7a94504f24b021876ad296ab7abf6a286e
Claus Nagel <cnagel@virtualcitysystems.de>
2019-12-27 09:49:54 +0100

    minor change

--- src/main/java/org/xmlobjects/gml/converter/GMLObjectConverter.java
+++ src/main/java/org/xmlobjects/gml/converter/GMLObjectConverter.java
@@ -3,6 +3,7 @@ package org.xmlobjects.gml.converter;
 import org.xmlobjects.gml.model.GMLObject;
 import org.xmlobjects.serializer.ObjectSerializeException;
 
+@FunctionalInterface
 public interface GMLObjectConverter<T extends GMLObject, R extends GMLObject> {
     R convert(T object) throws ObjectSerializeException;
 }
