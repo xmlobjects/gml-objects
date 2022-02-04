64fde12ebec8705d2d34ef0897b2cd729300343f
Claus Nagel <cnagel@virtualcitysystems.de>
2022-02-04 10:35:39 +0100

    minor change

--- src/main/java/org/xmlobjects/gml/util/Matrices.java
+++ src/main/java/org/xmlobjects/gml/util/Matrices.java
@@ -28,18 +28,20 @@ public class Matrices {
 
     public static Matrix newMatrix(List<Double> values, int m) {
         double[] tmp = new double[values.size()];
-        for (int i = 0; i < values.size(); i++)
+        for (int i = 0; i < values.size(); i++) {
             tmp[i] = values.get(i);
+        }
 
         return new Matrix(tmp, m);
     }
 
     public static List<Double> transform3D(CoordinateListProvider provider, Matrix matrix) {
-        if ((matrix.getRowDimension() != 3 && matrix.getRowDimension() != 4) || matrix.getColumnDimension() != 4)
+        if ((matrix.getRowDimension() != 3 && matrix.getRowDimension() != 4) || matrix.getColumnDimension() != 4) {
             throw new IllegalArgumentException("A 3D transformation requires either a 3x4 or a 4x4 matrix.");
+        }
 
         List<Double> coordinates = provider.toCoordinateList3D();
-        for (int i = 0; i < coordinates.size(); i +=3) {
+        for (int i = 0; i < coordinates.size(); i += 3) {
             Matrix v = new Matrix(new double[]{coordinates.get(i), coordinates.get(i + 1), coordinates.get(i + 2), 1}, 4);
             v = matrix.times(v);
 
