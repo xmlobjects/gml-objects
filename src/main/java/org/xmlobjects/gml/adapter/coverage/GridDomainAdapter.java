32a5e7b27924b41c23630b46af55096ac757c875
Claus Nagel <cnagel@virtualcitysystems.de>
2025-01-19 10:39:13 +0100

    updated copyright headers

--- src/main/java/org/xmlobjects/gml/adapter/coverage/GridDomainAdapter.java
+++ src/main/java/org/xmlobjects/gml/adapter/coverage/GridDomainAdapter.java
@@ -2,7 +2,7 @@
  * gml-objects - A Java mapping for the OGC Geography Markup Language (GML)
  * https://github.com/xmlobjects/gml-objects
  *
- * Copyright 2019-2024 Claus Nagel <claus.nagel@gmail.com>
+ * Copyright 2019-2025 Claus Nagel <claus.nagel@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
